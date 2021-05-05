73c9f733df00b8a9c7295cf53d99561398fbac2a
alex dong <alex.w.dong@gmail.com>
2021-05-05 01:21:51 -0400

    example on how to run the scripts

---
+++ scripts/preprocess_newsgroup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+
+threshold=.95
+mode=newsgroup
+data_dir='/home/jolteon/NLUProject/data/20news/'
+processed_dir='/home/jolteon/NLUProject/data/20news/processed/'
+python3='/mnt/e/miniconda3/envs/NLUProject/python.exe'
+conda activate NLUProject
+echo $python3
+echo $data_dir
+$python3 ./make_text_splits_dict.py -m $mode -d $data_dir -p $processed_dir
+$python3 ./make_cutoff_indices.py -t $threshold -m $mode -d $data_dir -p $processed_dir
+$python3 ./make_bert_encoded_segments.py -t $threshold -m $mode -d $data_dir -p $processed_dir
