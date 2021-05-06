82581a26a99b42297e3e02c9188f29b7d200120e
Shewtanshu Singh <shwetanshu.singh@nyu.edu>
2021-05-06 13:11:16 -0400

    removing scripts/.ipynb_checkpoints/*

--- scripts/.ipynb_checkpoints/download_data-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset,load_from_disk
-
-# BEFORE RUNNING: Make a symbolic link between ~/NLU_data and /scratch/[user]/NLU_data
-# example: ln -s /scratch/ay1626/NLU_data ~/NLU_data
-# also, mkdir ~/NLU_data/raw
-dataset = load_dataset('trivia_qa', 'rc',cache_dir = '~/NLU_data/raw')
-dataset.save_to_disk('~/NLU_data/raw/trivia_qa')
